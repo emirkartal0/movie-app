446d593ff06e907301428764a91fb35bcd18630d
emir <emirkartal00@gmail.com>
2023-06-06 21:10:44 +0300

    add search page

--- app/src/main/java/com/example/movieappgazi/SearchActivity.java
+++ app/src/main/java/com/example/movieappgazi/SearchActivity.java
@@ -1,15 +1,16 @@
 package com.example.movieappgazi;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.movieappgazi.models.Movie;
 import com.example.movieappgazi.request.Servicey;
@@ -25,22 +26,30 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+
     private List<Movie> movieList;
 
+    private RecyclerView recyclerView;
+    private SearchView searchView;
     BottomNavigationView nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.avtivity_search);
 
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        searchView = findViewById(R.id.searchView);
+
         nav = findViewById(R.id.nav);
+
+        nav.setSelectedItemId(R.id.search);
+
+        Log.d("src act","search activity cagirdik");
 
         nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -49,24 +58,32 @@ public class MainActivity extends AppCompatActivity {
 
                 int itemId = item.getItemId();
                 if (itemId == R.id.movies) {
-
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
-
                 } else if (itemId == R.id.search) {
-
                     Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
                     startActivity(intent);
-
                 }
 
                 return true;
             }
         });
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                queryMovies(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return false;
+            }
+        });
 
         movieList = new ArrayList<>();
-        fetchMovies();
 
 
     }
@@ -103,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
 
                     List<Movie> fetchedMovies = new ArrayList<>(response.body().getMovies());
 
-                    MovieAdapter movieAdapter = new MovieAdapter(MainActivity.this, fetchedMovies);
+                    MovieAdapter movieAdapter = new MovieAdapter(SearchActivity.this, fetchedMovies);
                     recyclerView.setAdapter(movieAdapter);
                 } else {
                     Log.v("Tag", "Error " + response.errorBody().toString());
