719989d431b2d4b880a5cc53c812c90f80554ba4
emir <emirkartal00@gmail.com>
2023-06-06 13:19:55 +0300

    add bottom navigation bar

--- app/src/main/java/com/example/movieappgazi/MainActivity.java
+++ app/src/main/java/com/example/movieappgazi/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.movieappgazi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.movieappgazi.models.Movie;
 import com.example.movieappgazi.request.Servicey;
 import com.example.movieappgazi.response.MovieSearchResponse;
 import com.example.movieappgazi.utils.Credentials;
 import com.example.movieappgazi.utils.MovieAPI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private List<Movie> movieList;
+
+    BottomNavigationView nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +39,29 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        nav = findViewById(R.id.nav);
+
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                int itemId = item.getItemId();
+                if (itemId == R.id.movies) {
+                    Toast.makeText(MainActivity.this, "Movies", Toast.LENGTH_LONG).show();
+                } else if (itemId == R.id.search) {
+                    Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_LONG).show();
+                    // Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                    // startActivity(intent);
+
+                } else if (itemId == R.id.favorites) {
+                    Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_LONG).show();
+                }
+
+                return true;
+            }
+        });
 
 
         movieList = new ArrayList<>();
